be99731518cb7e86ba3a1c1df5d4c7603f068c49
Freek Timmers <freektimmers@gmail.com>
2021-09-11 14:17:17 +0200

    Make connection with googleapis

--- pages/api/getSheet.ts
+++ pages/api/getSheet.ts
@@ -1,14 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next"
+import { getSheetsAPIClient } from "../../src/utility/getSheetsAPIClient"
+import { sheets_v4 } from "googleapis/build/src/apis/sheets/v4"
 
 type Data = {
-  name: string
+	name: boolean;
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+): Promise<void>
+{
+	let sheet: sheets_v4.Sheets = await getSheetsAPIClient()
+	sheet.spreadsheets
+
+	res.status(200).json({ name: !!sheet })
 }
